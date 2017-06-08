45c304be06bf0e7fbd58b0bd4115a355c8b0eb4a
Wilco Tomassen <wilco@wilcotomassen.nl>
2017-06-08 19:45:52 +0200

    Initial commit

---
+++ src/poster/Main.java
@@ -0,0 +1,24 @@
+package poster;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	
+	public void settings() {
+		// Add settings here
+		size(1024, 768);
+	}
+	
+	public void setup() {
+		// Add setup code here
+	}
+	
+	public void draw() {
+		// Add draw code here
+	}
+
+	public static void main(String[] args) {
+		// Program execution starts here
+		PApplet.main(Main.class.getName());
+	}
+
+}
